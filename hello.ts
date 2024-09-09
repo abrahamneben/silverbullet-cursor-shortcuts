53c45bc2b6bece0dd57a5e9f18eaae49b869a2db
Abraham Neben <abraham@gochromatic.com>
2024-09-09 09:18:36 -0400

    wip

--- hello.ts
+++ hello.ts
@@ -4,9 +4,9 @@ export async function helloWorld() {
   await editor.flashNotification("Hello world!");
 }
 
-export function moveCursor5LinesUp() {
+export async function moveCursor5LinesUp() {
   const pos = editor.getCursor();
   console.log(pos);
 
-  editor.moveCursor();
+  await editor.moveCursor();
 }
