d025710b8772e98c64737856ba902d15ef6ad008
Abraham Neben <abraham@gochromatic.com>
2024-09-09 09:20:16 -0400

    wip

--- hello.ts
+++ hello.ts
@@ -1,7 +1,7 @@
 import { editor } from "@silverbulletmd/silverbullet/syscalls";
 
 export async function helloWorld() {
-  await editor.flashNotification("Hello world!");
+  await editor.flashNotification("Hello world to abraham!");
 }
 
 export async function moveCursor5LinesUp() {
